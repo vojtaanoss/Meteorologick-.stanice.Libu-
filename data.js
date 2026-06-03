cabad65c248f5b33d35202db644d3bf9278da7ba
vojtaanoss <bachvojta2@gmail.com>
2026-06-03 12:58:34 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,649 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ze-strany",
+      "name": "Ze strany",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.07099503246197258,
+        "pitch": -0.08890142008950974,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4500985843124141,
+          "pitch": -0.0338744817513259,
+          "rotation": 0,
+          "target": "1-pedn-strana"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pedn-strana",
+      "name": "Přední strana",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.0013376625271632747,
+        "pitch": -0.03007726213891715,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31752660761062934,
+          "pitch": -0.029525425485296353,
+          "rotation": 0,
+          "target": "2-vnitek-1"
+        },
+        {
+          "yaw": 0.13000127617235613,
+          "pitch": -0.5869229675043321,
+          "rotation": 0,
+          "target": "5-stecha"
+        },
+        {
+          "yaw": -0.8508518559941134,
+          "pitch": -0.028255546726157377,
+          "rotation": 0,
+          "target": "6-vnitek-21"
+        },
+        {
+          "yaw": 0.9885756517512903,
+          "pitch": 0.09413990258288862,
+          "rotation": 0,
+          "target": "0-ze-strany"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.008173171984873662,
+          "pitch": -0.06759450519109933,
+          "title": "Měřící stanice kvality ovzduší<div><br></div>",
+          "text": "Jedná se o klimatizovaný kontejner ve kterém jsou umístěny přístroje na měření kvality ovzduší.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-vnitek-1",
+      "name": "Vnitřek 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7817885540989602,
+          "pitch": -0.028133525447845642,
+          "rotation": 0,
+          "target": "1-pedn-strana"
+        },
+        {
+          "yaw": -0.8411198489208829,
+          "pitch": 0.1763416271949687,
+          "rotation": 0,
+          "target": "3-vnitek-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vnitek-2",
+      "name": "Vnitřek 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7413569040176409,
+          "pitch": 0.1408700215384524,
+          "rotation": 0,
+          "target": "2-vnitek-1"
+        },
+        {
+          "yaw": -1.0791035743472825,
+          "pitch": 0.08354434418012957,
+          "rotation": 0,
+          "target": "4-vnitek-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vnitek-3",
+      "name": "Vnitřek 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.841760569103343,
+          "pitch": 0.13131645027473304,
+          "rotation": 0,
+          "target": "2-vnitek-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-stecha",
+      "name": "Střecha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5247024526965607,
+        "pitch": -0.1663269041102744,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.011384741510910956,
+          "pitch": 0.450854660420962,
+          "rotation": 0,
+          "target": "1-pedn-strana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6101923039347756,
+          "pitch": -0.09519532160122779,
+          "title": "Sondy pro měření prachových částic",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.4472411444205466,
+          "pitch": -0.7325970100931016,
+          "title": "Meteorologický stožár",
+          "text": "Je umístěn v 10 metrech nad zemí. Slouží k měření k rychlosti a směru větru."
+        }
+      ]
+    },
+    {
+      "id": "6-vnitek-21",
+      "name": "Vnitřek 2.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4336471910724313,
+        "pitch": 0.010615504284324118,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15637389583396555,
+          "pitch": 0.19068133704738877,
+          "rotation": 0,
+          "target": "7-vnitek-22"
+        },
+        {
+          "yaw": -2.5631102212298753,
+          "pitch": 0.08876112127568803,
+          "rotation": 0,
+          "target": "10-lev-strana"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-vnitek-22",
+      "name": "Vnitřek 2.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.3358478722582454,
+        "pitch": 0.048678854105833835,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6408203434003905,
+          "pitch": 0.16532294275162762,
+          "rotation": 0,
+          "target": "6-vnitek-21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-vnitek-31",
+      "name": "Vnitřek 3.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.30977923212390124,
+        "pitch": -0.039122206085323796,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.38588061474350255,
+          "pitch": 0.10988855055435565,
+          "rotation": 0,
+          "target": "9-vnitek-32"
+        },
+        {
+          "yaw": -3.1152719003076896,
+          "pitch": 0.10677469796629424,
+          "rotation": 0,
+          "target": "10-lev-strana"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-vnitek-32",
+      "name": "Vnitřek 3.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.7217918457866759,
+        "pitch": -0.4290161140152051,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2638451142298734,
+          "pitch": 0.1388681240978773,
+          "rotation": 0,
+          "target": "8-vnitek-31"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-lev-strana",
+      "name": "Levá strana",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.3224222437354065,
+        "pitch": -0.019579796667377636,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3254957647385268,
+          "pitch": 0.07459604324767355,
+          "rotation": 0,
+          "target": "1-pedn-strana"
+        },
+        {
+          "yaw": -1.4835923990061666,
+          "pitch": 0.042862707722264304,
+          "rotation": 0,
+          "target": "8-vnitek-31"
+        },
+        {
+          "yaw": -2.33812089597021,
+          "pitch": 0.03683302367330299,
+          "rotation": 0,
+          "target": "11-solrn-panely"
+        },
+        {
+          "yaw": -0.8147523834537331,
+          "pitch": 0.09131777345867675,
+          "rotation": 0,
+          "target": "6-vnitek-21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-solrn-panely",
+      "name": "Solární Panely",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3627431072323617,
+          "pitch": -0.0030926897275342924,
+          "rotation": 0,
+          "target": "12-solrn-panely-pedek"
+        },
+        {
+          "yaw": 0.9765684108635444,
+          "pitch": 0.04944856334432224,
+          "rotation": 0,
+          "target": "8-vnitek-31"
+        },
+        {
+          "yaw": 2.0414118339018748,
+          "pitch": 0.05798680604994466,
+          "rotation": 0,
+          "target": "10-lev-strana"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-solrn-panely-pedek",
+      "name": "Solární panely předek",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5067856994206554,
+          "pitch": 0.029113833124146282,
+          "rotation": 0,
+          "target": "11-solrn-panely"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Měřící stanice na Libuši VR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
